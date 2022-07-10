d2c13979b60acb2fb76ee21d4da90569f8c38def
Vitor Moraes <vitor.oliveira.moraes@gmail.com>
2022-07-10 20:05:16 -0300

    feat: example of funcional mqtt publish

--- huaweisolar.py
+++ huaweisolar.py
@@ -1,8 +1,10 @@
+'''
 import time
 from base_solar import HuaweiSolar
 import base_solar
 import paho.mqtt.client as mqtt
 import os
+
 import logging
 
 FORMAT = ('%(asctime)-15s %(threadName)-15s '
@@ -18,6 +20,7 @@ broker_port = os.getenv('BROKER_PORT', '1883')
 inverter = base_solar.HuaweiSolar(inverter_port, slave=1)
 inverter._slave = 1
 inverter.wait = 1
+
 
 #vars = ['state_1','state_2', 'state_3', 'alarm_1', 'alarm_2', 
 #'alarm_3', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage',
@@ -54,8 +57,11 @@ def settingUpMQTT():
     clientMQTT.on_connect=on_connect 
     clientMQTT.loop_start()
     log.info("Connecting to MQTT broker: %s ", mqtt_host)
-    clientMQTT.username_pw_set(username="", password="")
-    clientMQTT.connect(mqtt_host, broker_port) 
+    log.info("AQUI 0")
+    #clientMQTT.username_pw_set(username="", password="")
+    log.info("AQUI 1")
+    clientMQTT.connect(mqtt_host, 1883, 60) 
+    log.info("AQUI 2")
     while not clientMQTT.connected_flag: 
         log.info("...")
     time.sleep(1)
@@ -111,10 +117,43 @@ def modbusAccess(clientMQTT):
         # time.sleep(300) # 5min
         time.sleep(15) # 15seg
 
-# clientMQTT = settingUpMQTT()
-# modbusAccess(clientMQTT)
+#clientMQTT = settingUpMQTT()
+#modbusAccess(clientMQTT)
 
-clientMQTT = ""
-modbusAccess(clientMQTT)
+#clientMQTT = ""
+#modbusAccess(clientMQTT)
 
 # clientMQTT.loop_stop()
+'''
+
+#TEST
+'''
+import paho.mqtt.client as mqtt
+import time
+import logging
+
+FORMAT = ('%(asctime)-15s %(threadName)-15s '
+          '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+def on_connect(client, userdata, flags, rc):
+    time.sleep(10)
+    log.info(f"Connected with result code {rc}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+#client.username_pw_set(username="raspberrypie", password="rasp1234")
+#client.connect("7497a8feacd44088b913f9341d61a918.s1.eu.hivemq.cloud", 8883, 60) #Comunicação mosquitto
+
+# client.connect("192.168.100.108", 1883, 60) #Comunicação mosquitto
+for i in range(10000):
+    client.publish('raspberryTopic', payload=str(i), qos=0,
+    retain=False)
+    log.info(f"send {i} to raspberryTopic")
+    time.sleep(10)
+
+client.loop_forever()
+'''
+
