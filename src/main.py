1533c9dfd7433bd09a813e78d26505d239ec471e
Vitor Moraes <vitor.oliveira.moraes@gmail.com>
2022-07-14 09:08:07 -0300

    feat: function to get data and publish it

--- src/main.py
+++ src/main.py
@@ -1,60 +1,70 @@
 '''
 Info
 '''
-
-import os
 import time
 import json
 from libraries.docker_log import log_for_me
 from connection.mqtt import connect
 from data.inverter_data import IMMEDIATE_VARS, CALCULATED_VARS, get_data
 
-data_mode = 'OFFLINE' #Possibilites: 'OFFLINE' and 'INVERTER'
+DATA_MODE = 'INVERTER' #Possibilites: 'OFFLINE' and 'INVERTER'
+TOPIC = 'raspberryTopic'
 
-# def modbusAccess(clientMQTT):
-#     cont = 0
-#     while True:
-#         log_for_me("--> Started transmission")
-#         for i in IMMEDIATE_VARS:
-#             try:
-#                 mid = get_data(i)
-#                 log_for_me(i)
-#                 log_for_me(mid)
-#                 #clientMQTT.publish(topic="emon/NodeHuawei/"+i, 
-#                 #payload= str(mid.value), qos=1, retain=False)
-#             except:
-#                 pass
-#         if(cont > 0):
-#             for i in CALCULATED_VARS:
-#                 try:
-#                     mid = get_data(i)
-#                     log_for_me(i)
-#                     log_for_me(mid)
-#                     #clientMQTT.publish(topic="emon/NodeHuawei/"+i, 
-#                     #payload= str(mid.value), qos=1, retain=False)
-#                 except:
-#                     pass
-#             cont = 0
-#         cont = cont + 1 
-#         log_for_me("--> Ended transmission") 
-#         # time.sleep(300) # 5min
-#         time.sleep(15) # 15seg
+def send_data(client, storable_data):
+    try:
+        client.publish(TOPIC, payload=storable_data, qos=0, retain=False)
+    except:
+        log_for_me('ERROR PUBLISHING DATA TO MQTT BROKER')
 
-def get_solar_data():
-    data = {}
-    if data_mode == 'INVERTER':
-        data = {"Index": 0,"Type": "Inverter"}
-        data = json.dumps(data)
+def format_data_to_serialized_json(info):
+    log_for_me(info)
+    data = {
+                'ID': 0,
+                'Valor': 0,
+                'Aplicacao': 0,
+                'Local': 0,
+                'Tipo': 0,
+                'Variavel': 0,
+                'Unidade': 0,
+                'Rede': 0,
+                'Professor': 0
+            }
+    serialized_data = json.dumps(data)
+    return serialized_data
+
+def get_solar_data(variable):
+    if DATA_MODE == 'INVERTER':
+        return get_data(variable)
     else:
-        data = {"Index": 1,"Type": "Offline"}
-        data = json.dumps(data)
-    return data
+        log_for_me('To develop offiline data')
+        return '???'
+
+def pick_up_and_send_inverter_data(client):
+    log_for_me("--> Started transmission")
+    log_for_me("Getting Immediate variables")
+    for imm_var in IMMEDIATE_VARS:
+        try:
+            imm_response = get_solar_data(imm_var)
+            log_for_me(imm_var + ' | ' + str(imm_response))
+            json = format_data_to_serialized_json({'variable': imm_var, 'response': imm_response})
+            send_data(client, json)
+        except:
+            pass
+    log_for_me("Getting calculated variables")
+    for cal_var in CALCULATED_VARS:
+        try:
+            cal_response = get_solar_data(cal_var)
+            log_for_me(cal_var + ' | ' + str(cal_response))
+            json = format_data_to_serialized_json({'variable': cal_var, 'response': cal_response})
+            send_data(client, json)
+        except:
+            pass
+    log_for_me("--> Ended transmission") 
 
 client = connect()
+# --> add here function to wait to start in right time
 while True:
-    data = get_solar_data()
-    client.publish('raspberryTopic', payload=data, qos=0, retain=False)
-    log_for_me(f"send to raspberryTopic")
-    time.sleep(10)
-
-client.loop_forever()
+    response = pick_up_and_send_inverter_data(client)
+    time.sleep(5)
+    # time.sleep(300) # 5min
+# client.loop_forever()
