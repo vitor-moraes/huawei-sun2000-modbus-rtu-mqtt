622f1edc5cf5d3752c7c3563603b7c8035b18a18
Vitor Moraes <vitor.oliveira.moraes@gmail.com>
2022-07-22 14:10:56 -0300

    fix: log format

--- src/main.py
+++ src/main.py
@@ -59,7 +59,7 @@ def pick_up_and_send_inverter_data(client):
     for cal_var in CALCULATED_VARS:
         try:
             cal_response = get_solar_data(cal_var)
-            log_info(cal_var + ' | ' + str(cal_response))
+            log_info(imm_var + ' = ' + str(imm_response.value) + " " + str(imm_response.unit))
             json = format_data_to_serialized_json({'variable': cal_var,
                                                    'value': cal_response.value,
                                                    'unit': cal_response.unit})
